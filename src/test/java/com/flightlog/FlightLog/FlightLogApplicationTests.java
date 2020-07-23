9abc10799f0d5c0c7c2fc79c6cac7d59c5e604f1
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-23 14:37:27 -0400

    init

---
+++ src/test/java/com/flightlog/FlightLog/FlightLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightlog.FlightLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
