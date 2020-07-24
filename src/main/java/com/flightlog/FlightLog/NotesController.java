979a3f9902e10bfa3b122c332bb0518fa80c2d07
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-24 12:21:31 -0400

    all endpoints MVP

--- src/main/java/com/flightlog/FlightLog/NotesController.java
+++ src/main/java/com/flightlog/FlightLog/NotesController.java
@@ -2,6 +2,8 @@ package com.flightlog.FlightLog;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/notes")
 public class NotesController {
@@ -18,11 +20,39 @@ public class NotesController {
     }
 
     @PostMapping("/{flightid}")
-    public Object addNote(@PathVariable Long id, @RequestBody String bodynote){
-          Notes note = new Notes();
-//        Flight flight = new Flight();
+    public Object addNote(@PathVariable Long flightid, @RequestBody(required=false) Notes bodynote) throws NullPointerException{
 
-            note.setFlight(id);
+        try{
+            String test = bodynote.getNotes();
+            System.out.println(bodynote);
+            System.out.println(test);
+            if(test == null){
+                return "No notes in request body";
+            }
+        } catch (NullPointerException ex){
+            return "No body found";
+        }
 
+        Notes note = new Notes();
+          Optional<Flight> flight = flightrepository.findById(flightid);;
+          if(flight.isPresent() && (bodynote != null && !bodynote.getNotes().equals(""))){
+
+              Flight flight1 = flight.get();
+
+              Optional<Pilot> pilot = pilotrepository.findById(flight1.getPilot().getId());
+
+              if(pilot.isPresent()){
+                  note.setFlight(flight1);
+                  Pilot pilot1 = pilot.get();
+                  note.setPilot(pilot1);
+                  note.setNotes(bodynote.getNotes());
+
+                  return this.noterepository.save(note);
+              }
+
+
+          }
+
+        return "Something went wrong...";
     }
 }
